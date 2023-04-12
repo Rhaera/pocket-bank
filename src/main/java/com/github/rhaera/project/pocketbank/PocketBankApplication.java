402bb1140b4246a3b4193fbf0679592ea461571c
Rhaera <rauldantas@usp.br>
2023-04-12 20:47:58 -0300

    Application changes

--- src/main/java/com/github/rhaera/project/pocketbank/PocketBankApplication.java
+++ src/main/java/com/github/rhaera/project/pocketbank/PocketBankApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PocketBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocketBankApplication.class, args);
-	}
 
+		SpringApplication.run(PocketBankApplication.class, args);
+
+	}
 }
